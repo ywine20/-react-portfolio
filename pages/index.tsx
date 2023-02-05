ab4f5a82c4cd837a5581234adb7bc548acdf87c4
Y wine <118164991+ywine20@users.noreply.github.com>
2023-02-05 23:11:38 +0630

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,50 +22,7 @@ import web8 from "../public/web8.png";
 import { useState } from "react";
 export default function Home() {
    const [darkMode, setDarkMode] = useState(false);
- const [status, setStatus] = useState({
-        submitted: false,
-        submitting: false,
-        info: { error: false, msg: null },
-    });
-
-    const [inputs, setInputs] = useState({
-        email: '',
-        message: '',
-    });
-
-    const handleResponse = (status, msg) => {
-        if (status === 200) {
-            setStatus({
-                submitted: true,
-                submitting: false,
-                info: { error: false, msg: msg },
-            });
-            setInputs({
-                email: '',
-                message: '',
-            });
-        } else {
-            setStatus({
-                info: { error: true, msg: msg },
-            });
-        }
-    };
-
-    };
-
-    const handleOnSubmit = async (e) => {
-        e.preventDefault();
-        setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
-        const res = await fetch('/api/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(inputs),
-        });
-        const text = await res.text();
-        handleResponse(res.status, text);
-    };
+ 
   
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -249,67 +206,7 @@ export default function Home() {
                 className="rounded-lg object-cover"src={web8}
               />
             </div>
-              
-            
-            
-
-          </div>
-          <form
-                    onSubmit={handleOnSubmit}
-                    className="relative w-full px-4 mx-auto max-w-lg"
-                >
-                    <h1 className="relative text-center font-light text-3xl leading-relaxed lg:text-5xl">
-                        <span className="uppercase font-bold tracking-10 text-teal-500">
-                            Contact me
-                        </span>
-                    </h1>
-
-                    <div className="mt-10">
-                        <label className='text-teal-500' htmlFor="email">Email</label>
-                        <input
-                            className="block w-full p-2 bg-gray-800 border border-white focus:outline-none"
-                            id="email"
-                            type="email"
-                            onChange={handleOnChange}
-                            required
-                            placeholder="your@email.com"
-                            value={inputs.email}
-                        />
-                    </div>
-                    <div className="mt-10">
-                        <label className='text-teal-500' htmlFor="message">Message</label>
-                        <textarea
-                            className="block w-full p-2 bg-gray-800 border border-white focus:outline-none"
-                            id="message"
-                            onChange={handleOnChange}
-                            required
-                            rows={10}
-                            value={inputs.message}
-                            placeholder="Your message..."
-                        />
-                    </div>
-                    <button
-                        className="mt-10 py-2 px-4 border-2 border-white text-white uppercase text-sm tracking-24 bg-transparent hover:bg-white hover:text-gray-900 transition duration-150 ease-in-out"
-                        type="submit"
-                        disabled={status.submitting}
-                    >
-                        {!status.submitting
-                            ? !status.submitted
-                                ? 'Submit'
-                                : 'Submitted'
-                            : 'Submitting...'}
-                    </button>
-                    <div className="mt-4">
-                        {status.info.error && (
-                            <div className="error">
-                                Error: {status.info.msg}
-                            </div>
-                        )}
-                        {!status.info.error && status.info.msg && (
-                            <div className="success">{status.info.msg}</div>
-                        )}
-                    </div>
-                </form>
+             </div>
         </section>
       </main>
     </div>
